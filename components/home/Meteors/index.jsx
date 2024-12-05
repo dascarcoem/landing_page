04a1b631e66c559e906c6816d5e1af67765001c0
Adeesh-bode <bodead@rknec.edu>
2024-12-06 04:38:23 +0530

    objective and metor section done

---
+++ components/home/Meteors/index.jsx
@@ -0,0 +1,12 @@
+import MeteorsBg from '@/components/ui/Meteors'
+import React from 'react'
+
+const Meteors = () => {
+  return (
+    <div className=' relative h-[35vh]' >
+        <MeteorsBg number={50} />
+    </div>
+  )
+}
+
+export default Meteors
