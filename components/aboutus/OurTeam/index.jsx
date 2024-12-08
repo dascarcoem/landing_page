2e85b090758dc4685c3b81574e1164ef681b7f19
Adeesh-bode <bodead@rknec.edu>
2024-12-08 16:20:32 +0530

    changes

--- components/aboutus/OurTeam/index.jsx
+++ components/aboutus/OurTeam/index.jsx
@@ -6,7 +6,7 @@ const OurTeam = () => {
     <div className='w-full h-screen flex flex-col text-4xl px-[calc(100vw*0.2)] py-[calc(100vw*0.2)] ' >
       <h1 className='flex justify-center items-end gap-2 lg:gap-3 text-7xl' >
         <span className='text-8xl' >M</span>
-        <GradualSpacing text='eet Our Dynamic Team'  className={'pb-1'} />
+        <GradualSpacing text='eet Our Dynamic Team' className={'pb-1'} />
       </h1>
     </div>
   )
--- components/home/Partners/indes.jsx
+++ components/home/Partners/indes.jsx
@@ -1,9 +1,97 @@
-import React from 'react'
+'use client'
+import React, { useEffect } from 'react';
+import { gsap } from 'gsap';
+import { ScrollTrigger } from 'gsap/ScrollTrigger';
+
+gsap.registerPlugin(ScrollTrigger);
 
 const Partners = () => {
-  return (
-    <div className='w-full h-screen center' >Partners</div>
-  )
-}
+  useEffect(() => {
+    const exp = gsap.timeline({
+      scrollTrigger: {
+        trigger: '.experience',
+        start: 'top top',
+        end: '+=5000',
+        scrub: true,
+        // markers: true,
+        pin: '.experience',
+      },
+    });
 
-export default Partners
+    exp.to('.experience-middle', {
+      '--progress1': 1,
+      duration: 5,
+      smoothOrigin: true,
+    });
+  }, []);
+
+  return (
+    <>
+      <style>{`
+        :root {
+          --progress1: 0;
+        }
+
+        .experience {
+          position: relative;
+          width: 100%;
+          height: 100vh;
+          z-index: 10;
+          overflow: hidden;
+        }
+
+        .experience .experience-top {
+          position: absolute;
+          top: 1%;
+          left: 2%;
+          font-size: 3rem;
+          font-family: bold;
+        }
+
+        .experience .experience-middle-container {
+          width: 100%;
+          height: 100vh;
+          display: flex;
+          justify-content: center;
+          align-items: center;
+        }
+
+        .experience .experience-middle {
+          transform: scale(calc(var(--progress1) * 8.5)) !important;
+          transform-origin: center;
+          font-size: 3rem;
+          font-family: bold;
+        }
+
+        .experience .experience-bottom {
+          position: absolute;
+          bottom: 1%;
+          right: 2%;
+          font-size: 3rem;
+          font-family: bold;
+        }
+
+        section {
+          height: 100vh;
+          width: 100%;
+          margin-top: 1rem;
+          font-size: 5rem;
+        }
+      `}</style>
+
+      <div className="experience">
+        <div className="experience-show">
+          <h1 className="experience-top">Let Me show you</h1>
+          <div className="experience-middle-container">
+            <h1 className="experience-middle">Enter My World</h1>
+          </div>
+          <h1 className="experience-bottom">My Magic Trick</h1>
+          <div className="spacer-end"></div>
+        </div>
+      </div>
+      <section>another section</section>
+    </>
+  );
+};
+
+export default Partners;
